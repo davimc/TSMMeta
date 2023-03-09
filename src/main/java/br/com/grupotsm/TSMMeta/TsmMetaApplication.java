a6c225a7650a6346d50011cf2351ff4a988c7a51
davimc <davimatosc@hotmail.com>
2023-03-09 01:19:35 -0300

    Initial commit

---
+++ src/main/java/br/com/grupotsm/TSMMeta/TsmMetaApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupotsm.TSMMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsmMetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grupotsm/TSMMeta/TsmMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupotsm.TSMMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsmMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
