721c92568b6fb5d7ba7e06d7f5b50fc8afdd34f9
davimc <davimatosc@hotmail.com>
2023-03-10 20:44:47 -0300

    Spring security

--- src/main/java/br/com/grupotsm/TSMMeta/config/SecurityConfig.java
+++ src/main/java/br/com/grupotsm/TSMMeta/config/SecurityConfig.java
@@ -1,18 +1,32 @@
 package br.com.grupotsm.TSMMeta.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -24,7 +38,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/authentication/**").permitAll();
                     auth.requestMatchers("/h2-console/**").permitAll();
-                    auth.anyRequest().authenticated();
+                    auth.requestMatchers("/tests").permitAll();
+                    auth.anyRequest().permitAll();
                 })
                 .build();
     }
--- src/main/java/br/com/grupotsm/TSMMeta/services/UserService.java
+++ src/main/java/br/com/grupotsm/TSMMeta/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -14,8 +15,8 @@ import java.util.Collection;
 @Service
 public class UserService implements UserDetailsService {
 
+    @Autowired
     private final UserRepository repository;
-
     @Autowired
     public UserService(UserRepository userRepository) {
         this.repository = userRepository;
