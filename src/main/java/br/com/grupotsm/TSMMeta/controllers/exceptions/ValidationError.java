c75bf711b114084e061e38f9534602c6065407bf
davimc <davimatosc@hotmail.com>
2023-04-23 06:20:46 -0300

    fix

--- src/main/java/br/com/grupotsm/TSMMeta/controllers/exceptions/ValidationError.java
+++ src/main/java/br/com/grupotsm/TSMMeta/controllers/exceptions/ValidationError.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class ValidationError extends StandardError {
     private static final Long serialVersionUID = 1L;
-    private List<FieldMessage> erros = new ArrayList<>();
+    private List<FieldMessage> errors = new ArrayList<>();
 
-    public List<FieldMessage> getErros() {
-        return erros;
+    public List<FieldMessage> getErrors() {
+        return errors;
     }
 
     public void addError(String fieldName, String message) {
-        erros.add(new FieldMessage(fieldName, message));
+        errors.add(new FieldMessage(fieldName, message));
     }
 }
